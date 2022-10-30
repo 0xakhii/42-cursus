eb9c2b5ca0a4230a3d521f577b29b2d4bc3aa05a
Omar Jamal <ojamal@e2r2p10.1337.ma>
2022-10-30 08:51:04 +0100

    added split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,18 +6,70 @@
 /*   By: ojamal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 10:01:54 by ojamal            #+#    #+#             */
-/*   Updated: 2022/10/25 02:52:25 by ojamal           ###   ########.fr       */
+/*   Updated: 2022/10/30 08:47:30 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// char	**ft_split(char const *s, char c)
-// {
-// }
+int	wrd_cnt(char const *s, char del)
+{
+	int	index;
+	int	count;
 
-// int main()
+	index = 0;
+	count = 0;
+	while (s[index] != '\0')
+	{
+		while (s[index] == del)
+			index++;
+		if (s[index] != '\0')
+			count++;
+		while (s[index] != del && s[index] != '\0')
+			index++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+	char	**str;
+
+	l = wrd_cnt(s, c);
+	i = 0;
+	k = 0;
+	str = (char **)malloc((wrd_cnt(s, c) + 1) * sizeof(char *));
+	if (!str)
+		return (NULL);
+	while (k < l)
+	{
+		while (s[i] == c && s[i] != '\0')
+			i++;
+		j = i;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		str[k] = ft_substr(s, j, i - j);
+		k++;
+	}
+	str[k] = NULL;
+	return (str);
+}
+
+// #include<stdio.h>
+// int	main()
 // {
-// 	char str[] = "hello hello hello";
-// 	printf("%d\n", word_count(str, ' '));
+// 	char	str[] = "        J FE MAR APRI MAY JUN JULY AUG 
+//	SEPTEMBER OCTOB NOVEMB DEC ";
+// 	char **A = ft_split(str, ' ');
+// 	int	i = 0;
+// 	while (i < 12)
+// 	{
+// 		printf("%s\n", A[i]);
+// 		i++;
+// 	}
+// 	system("leaks a.out");
 // }
