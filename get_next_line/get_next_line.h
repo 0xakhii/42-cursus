f4d9f31e5b0f17d4685ef885111a3035f338e110
cat161 <ojamal710@gmail.com>
2022-11-09 07:27:03 +0100

    added a func

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,17 @@
 /*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:56:51 by ojamal            #+#    #+#             */
-/*   Updated: 2022/11/08 17:02:10 by ojamal           ###   ########.fr       */
+/*   Updated: 2022/11/09 07:26:17 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include<stdlib.h>
+
 char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+
 
 #endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,8 +6,36 @@
 /*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:54:21 by ojamal            #+#    #+#             */
-/*   Updated: 2022/11/08 16:57:59 by ojamal           ###   ########.fr       */
+/*   Updated: 2022/11/09 07:23:53 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		index;
+	int		index2;
+
+	index = 0;
+	index2 = 0;
+	if (s1 == '\0' || s2 == '\0')
+		return (NULL);
+	str = malloc ((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s1[index])
+	{
+		str[index2++] = s1[index];
+		index++;
+	}
+	index = 0;
+	while (s2[index])
+	{
+		str[index2++] = s2[index];
+		index++;
+	}
+	str[index2] = '\0';
+	return (str);
+}
