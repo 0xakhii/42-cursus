58ab3c8e38e4f748da736f656870050970fe0a47
cat161 <ojamal@student.1337.ma>
2023-03-02 05:30:14 +0100

    added philo

---
+++ Philosophers/philo/philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojamal <ojamal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 04:31:02 by ojamal            #+#    #+#             */
+/*   Updated: 2023/03/02 05:29:53 by ojamal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+// typedef struct s_philo
+// {
+	
+// }			t_philo;
+
+
+#endif
