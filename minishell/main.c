50d9aca1272e6656c890936247b6d7532a0a1758
Omar Jamal <ojamal@e2r10p1.1337.ma>
2023-05-26 20:50:01 +0100

    fixing syntax

--- minishell/main.c
+++ minishell/main.c
@@ -6,182 +6,170 @@
 /*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 16:42:56 by ojamal            #+#    #+#             */
-/*   Updated: 2023/05/25 19:48:21 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/05/26 20:49:10 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char    *add_characthers(char *str, char x)
+char	*add_characthers(char *str, char x)
 {
-    char *new_str;
-    int i = 0;
-    if (str == NULL)
-    {
-        new_str = malloc(2);
-        new_str[0] = x;
-        new_str[1] = '\0'; 
-    }
-    else
-    {
-        new_str = malloc(ft_strlen(str) + 2);
-        while (str[i])
-        {
-            new_str[i] = str[i];
-            i++;
-        }
-        new_str[i] = x;
-        new_str[i + 1] = '\0';
-    }
-    return (new_str);
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	if (str == NULL)
+	{
+		new_str = malloc(2);
+		new_str[0] = x;
+		new_str[1] = '\0';
+	}
+	else
+	{
+		new_str = malloc(ft_strlen(str) + 2);
+		while (str[i])
+		{
+			new_str[i] = str[i];
+			i++;
+		}
+		new_str[i] = x;
+		new_str[i + 1] = '\0';
+	}
+	return (new_str);
 }
 
-int is_empty(t_list *lexer)
+int	quote_check(t_list *lexer)
 {
-    char *str;
-    while (lexer)
-    {
-        str = lexer->content;
-        if (str[0] != ' ' && str[0] != '\t')
-            return (0);
-        lexer = lexer->next;
-    }
-    return (1);
+	int		s_quote;
+	int		d_quote;
+	char	*str;
+	int		i;
+
+	s_quote = 0;
+	d_quote = 0;
+	while (lexer)
+	{
+		str = lexer->content;
+		i = 0;
+		while (str[i])
+		{
+			if (str[i] == '\'')
+				s_quote = !s_quote;
+			if (str[i] == '\"')
+				d_quote = !d_quote;
+			i++;
+		}
+		lexer = lexer->next;
+	}
+	if (s_quote || d_quote)
+		return (1);
+	return (0);
 }
 
-int quote_check(t_list *lexer)
+t_list	*lexer_init(char *in)
 {
-    int singleQuoteOpen = 0;
-    int doubleQuoteOpen = 0;
+	int		i;
+	char	*str;
+	t_list	*lexer;
 
-    while (lexer)
-    {
-        char *str = lexer->content;
-        int i = 0;
-
-        while (str[i])
-        {
-            if (str[i] == '\'')
-                singleQuoteOpen = !singleQuoteOpen;
-
-            if (str[i] == '\"')
-                doubleQuoteOpen = !doubleQuoteOpen;
-
-            i++;
-        }
-
-        lexer = lexer->next;
-    }
-
-    if (singleQuoteOpen || doubleQuoteOpen)
-        return 1;
-
-    return 0;
+	lexer = NULL;
+	i = 0;
+	while (1)
+	{
+		if (in[i] == '\0' || in[i] == '\n')
+			break ;
+		while (in[i])
+		{
+			str = NULL;
+			if (ft_isalpha(in[i]) || in[i] == ' ' || in[i] == '\t')
+			{
+				while (in[i] && (ft_isalpha(in[i]) || in[i] == ' '
+						|| in[i] == '\t'))
+				{
+					str = add_characthers(str, in[i]);
+					i++;
+				}
+				ft_lstadd_back(&lexer, ft_lstnew(str));
+			}
+			else
+			{
+				if (in[i] == '<')
+				{
+					if (in[i + 1] == '<')
+					{
+						str = malloc(3);
+						str[0] = '<';
+						str[1] = '<';
+						str[2] = '\0';
+						i += 2;
+					}
+					else
+					{
+						str = malloc(2);
+						str[0] = '<';
+						str[1] = '\0';
+						i++;
+					}
+				}
+				else if (in[i] == '>')
+				{
+					if (in[i + 1] == '>')
+					{
+						str = malloc(3);
+						str[0] = '>';
+						str[1] = '>';
+						str[2] = '\0';
+						i += 2;
+					}
+					else
+					{
+						str = malloc(2);
+						str[0] = '>';
+						str[1] = '\0';
+						i++;
+					}
+				}
+				else if (in[i] == '|')
+				{
+					str = malloc(2);
+					str[0] = '|';
+					str[1] = '\0';
+					i++;
+				}
+				else
+				{
+					str = malloc(2);
+					str[0] = in[i];
+					str[1] = '\0';
+					i++;
+				}
+				ft_lstadd_back(&lexer, ft_lstnew(str));
+			}
+			if (quote_check(lexer))
+			{
+				printf("quote error\n");
+				break ;
+			}
+		}
+	}
+	return (lexer);
 }
 
-t_list *lexer_init(char *in)
+int	main(int ac, char **av, char **env)
 {
-    int i;
-    char *str;
-    t_list *lexer;
+	(void)ac;
+	(void)av;
+	(void)env;
+	t_list	*lexer;
+	char	*in;
 
-    lexer = NULL;
-    i = 0;
-    while (1)
-    {
-        if (in[i] == '\0')
-            break;
-        while (in[i])
-        {
-            str = NULL;
-            if (ft_isalpha(in[i]) || in[i] == ' ' || in[i] == '\t')
-            {
-                while (in[i] && (ft_isalpha(in[i]) || in[i] == ' ' || in[i] == '\t'))
-                {
-                    str = add_characthers(str, in[i]);
-                    i++;
-                }
-                ft_lstadd_back(&lexer, ft_lstnew(str));
-            }
-            else
-            {
-                if (in[i] == '<')
-                {
-                    if (in[i + 1] == '<')
-                    {
-                        str = malloc(3);
-                        str[0] = '<';
-                        str[1] = '<';
-                        str[2] = '\0';
-                        i += 2;
-                    }
-                    else
-                    {
-                        str = malloc(2);
-                        str[0] = '<';
-                        str[1] = '\0';
-                        i++;
-                    }
-                }
-                else if (in[i] == '>')
-                {
-                    if (in[i + 1] == '>')
-                    {
-                        str= malloc(3);
-                        str[0] = '>';
-                        str[1] = '>';
-                        str[2] = '\0';
-                        i+=2;
-                    }
-                    else
-                    {
-                        str = malloc(2);
-                        str[0] = '>';
-                        str[1] = '\0';
-                        i++;
-                    }
-                }
-                else if (in[i] == '|')
-                {
-                    str = malloc(2);
-                    str[0] = '|';
-                    str[1] = '\0';
-                    i++;
-                }
-                else
-                {
-                    str = malloc(2);
-                    str[0] = in[i];
-                    str[1] = '\0';
-                    i++;
-                }
-                ft_lstadd_back(&lexer, ft_lstnew(str));
-            }
-            if (quote_check(lexer))
-            {
-                printf("quote error\n");
-                break;
-            }
-            if (is_empty(lexer))
-            {
-                // printf("empty error\n");
-                break;
-            }
-        }
-    }
-    return (lexer);
-}
-
-int	main(void)
-{
-    t_list *lexer;
-    char *in;
-
-    lexer = NULL;
-    while (1)
-    {
-        in = readline("minishell$>");
-        lexer = lexer_init(in);
-        printing(lexer);
-    }
+	lexer = NULL;
+	while (1)
+	{
+		in = readline("minishell$>");
+		if (in)
+			add_history(in);
+		lexer = lexer_init(in);
+		printing(lexer);
+	}
 }
